9f530480b4fe0293b283ead8616c07fc4459b59b
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-12-08 14:59:02 +0200

    test: add app/test page to verify Vercel app routing

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{padding:32}}>
+      <h1>Deployment Test Page</h1>
+      <p>If you see this, the app router is being served correctly.</p>
+    </div>
+  )
+}
